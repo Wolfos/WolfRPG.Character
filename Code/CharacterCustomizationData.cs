8d17a073edde560b38f5b371a9b6b8645689a812
Robin <robin@rvanee.nl>
2023-08-13 00:14:33 +0200

    Move character customization data model to WolfRPG.Character

---
+++ Code/CharacterCustomizationData.cs
@@ -0,0 +1,31 @@
+﻿namespace WolfRPG.Character
+{
+	public enum Gender
+	{
+		Female, Male
+	}
+	public enum CharacterCustomizationPart
+	{
+		Gender, Hair, BackAttachment, Head, Eyebrows, FacialHair, Torso, ArmUpperRight, ArmUpperLeft, ArmLowerRight, ArmLowerLeft, HandRight, HandLeft, Hips, LegRight, LegLeft
+	}
+	
+	public struct CharacterCustomizationData
+	{
+		public Gender Gender;
+		public int Hair;
+		public int BackAttachment;
+		public int Head;
+		public int Eyebrows;
+		public int FacialHair;
+		public int Torso;
+		public int ArmUpperRight;
+		public int ArmUpperLeft;
+		public int ArmLowerRight;
+		public int ArmLowerLeft;
+		public int HandRight;
+		public int HandLeft;
+		public int Hips;
+		public int LegRight;
+		public int LegLeft;
+	}
+}
